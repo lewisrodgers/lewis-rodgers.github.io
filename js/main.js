74908414c8e1331fa06af080d98d95d61f60b9a5
Lewis Rodgers <lrodgersdesign@gmail.com>
2015-07-19 20:49:30 -0400

    Fix user Id
    The instagram userId value has to be a number and not a string.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var feed = new Instafeed({
     get: 'user',
-    userId: '1917759821',
+    userId: 1917759821,
     accessToken: '1917759821.467ede5.39d74d1b52204a81b68935140c3ca06a'
 });
 feed.run();
