4ced5d860091564e2152bdedd9b3040fc58b9ef5
Lewis Rodgers <contact@lewisrodgers.com>
2017-03-29 07:17:35 -0700

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var feed = new Instafeed({
     get: 'user',
     userId: 1156934118,
-    accessToken: '1156934118.8eba533.3d3a693fe077484da336501c4b419949'
+    clientId: '8eba5336df614c4ea22fbc6af450dc6d'
+    //accessToken: '1156934118.8eba533.3d3a693fe077484da336501c4b419949'
 });
 feed.run();
